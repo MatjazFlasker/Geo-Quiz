13b267a1eb259e64f0a979c0c25aec39a37c43a2
MatjazFlasker <33814397+MatjazFlasker@users.noreply.github.com>
2024-05-28 07:17:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+
+countries_cities = {"Austria": "Vienna",
+                    "Belgium": "Brussels",
+                    "Bulgaria": "Sofia",
+                    "Croatia": "Zagreb",
+                    "Cyprus": "Nicosia",
+                    "Czech Republic": "Prague",
+                    "Denmark": "Copenhagen",
+                    "Estonia": "Tallinn",
+                    "Finland": "Helsinki",
+                    "France": "Paris",
+                    "Germany": "Berlin",
+                    "Greece": "Athens",
+                    "Hungary": "Budapest",
+                    "Ireland": "Dublin",
+                    "Italy": "Rome",
+                    "Latvia": "Riga",
+                    "Lithuania": "Vilnius",
+                    "Luxembourg": "Luxembourg",
+                    "Malta": "Valletta",
+                    "Netherlands": "Amsterdam",
+                    "Poland": "Warsaw",
+                    "Portugal": "Lisbon",
+                    "Romania": "Bucharest",
+                    "Slovakia": "Bratislava",
+                    "Slovenia": "Ljubljana",
+                    "Spain": "Madrid",
+                    "Sweden": "Stockholm"}
+
+def geo_quiz():
+    country = random.choice(list(countries_cities.keys()))
+    capital = countries_cities[country]
+
+    print(f"What is the capital city of: {country}?")
+    answer = input("Answer: ")
+
+    if answer.lower() == capital.lower():
+        print("Correct answer!")
+    
+    else:
+        print(f"Wrong answer! The capital city of {country} is {capital}.")
+
+    user_input = input("Do you want to guess again? (Y/N) ")
+
+    if user_input.lower() == "y":
+        geo_quiz()
+    
+    else:
+        print("Thanks for playing!")
+
+geo_quiz()
